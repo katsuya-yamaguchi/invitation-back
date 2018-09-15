57a3a20ae7bddf33c1cc413765d2b12e3b09e859
Katsuya003 <bb.win.3@gmail.com>
2018-09-15 11:55:56 +0900

    [update]

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,15 +2,15 @@ class UsersController < ApplicationController
   #before_action :set_user, only: [:show, :update, :destroy]
 
   # POST /user
-  #def create
-  #  @user = User.new(user_params)
-
-  #  if @user.save
-  #    render json: @user, status: :created, location: @user
-  #  else
-  #    render json: @user.errors, status: :unprocessable_entity
-  #  end
-  #end
+  def create
+    data = request.body.string
+    user = User.new
+    if user.changeAlelgyStatus(data) == 0 then
+      render nothing: true, status: 200
+    elsif 
+      render nothing: true, status: 400
+    end
+  end
 
   # PATCH/PUT /user
   def update
--- app/models/user.rb
+++ app/models/user.rb
@@ -16,4 +16,21 @@ class User < ApplicationRecord
       return 1
     end
   end
+
+  def changeAlelgyStatus(json_data)
+    data = JSON.parse(json_data)
+    token = data['token']
+    status = data['status']
+    user_data = decode(token)
+    email = user_data['email']
+    user_id = user_data['user_id']
+    user = User.where(email: email)
+    begin
+      user.update_all(user_allergy: status, sid: user_id)
+      return 0
+    rescue => error
+      p error
+      return 1
+    end
+  end
 end
