920c9c12b140f790685afad61b17b52984b3a58a
Katsuya003 <bb.win.3@gmail.com>
2018-11-11 14:52:27 +0900

    [update] models/user.rb に関するテストを追加
    attend_status、allelgy_statusを更新する処理のテストを追加。

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,22 +1,72 @@
 require 'rails_helper'
-
-require 'net/http'
-require 'uri'
 require 'json'
-require 'jwt'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'change_attend_status' do
+    let(:json_data){
+      json_data = {
+        email: 'test@test.com',
+        user_id: '0123456789',
+        status: '0'
+      }
+      JSON.generate(json_data)
+    }
 
-  describe '#changeAttendStatus' do
-    it 'Status of attend is updated.' do
-      token = 'eyJhbGciOiJSUzI1NiIsImtpZCI6IjBmNTVkZWZlOWU5YzU2ZmRhZTRkOGY0MDFjZjQ5Njc4YzE2N2MzYWEifQ.eyJpc3MiOiJodHRwczovL3NlY3VyZXRva2VuLmdvb2dsZS5jb20vaW52aXRhdGlvbi1jYTRiMiIsImF1ZCI6Imludml0YXRpb24tY2E0YjIiLCJhdXRoX3RpbWUiOjE1MzY2MDg4OTAsInVzZXJfaWQiOiJCQm8yOU9IOEtXUlQ4dmZPTTBJRVdvTzdVTmMyIiwic3ViIjoiQkJvMjlPSDhLV1JUOHZmT00wSUVXb083VU5jMiIsImlhdCI6MTUzNjgzODI1NSwiZXhwIjoxNTM2ODQxODU1LCJlbWFpbCI6ImJiLndpbi4zQGdtYWlsLmNvbSIsImVtYWlsX3ZlcmlmaWVkIjpmYWxzZSwiZmlyZWJhc2UiOnsiaWRlbnRpdGllcyI6eyJlbWFpbCI6WyJiYi53aW4uM0BnbWFpbC5jb20iXX0sInNpZ25faW5fcHJvdmlkZXIiOiJwYXNzd29yZCJ9fQ.GkJiYLNuvSU19OaCQ1ZtaBN9Vd5yrYBkLqH6UHQ1NzAw85wgECqMC0RZePNdb7NMwntHcwEVMhmSwTNmJtjZUXB5KMiVMoxEr97AetZZDJfeFiqepOjyL9qbF22nzrTqHY5DdyLU3kG_h_SWLFkGUzxFEa_rBFs8J80jkN6aywhm2jKf396OEn_pU4bpMrJly8X7BWrEIDxjo9edU0cA5KWbjryXE8IQBiAu8mBGK_ZlAfQLTZMYxLtrEbkOmLxY0q546bVcgef8wyBu-VEJJsze4aNQq1AfIQwe4xGP8ITJQvOnaWD1ndZayiz6iwXfEsmceAqGCeLqqOxQ1qvuXw'
-      status = 1
-      json_data = {'token': token, 'status': status}
-      user = User.new(email: 'bb.win.3@gmail.com')
-      user.save!
-      user.changeAttendStatus(JSON.generate(json_data))
-      expect( User.where(email: 'bb.win.3@gmail.com')[0].attributes['attend_status'] ).to eq 1
+    context 'when attend_status update failed,' do
+      it 'returns 1.' do
+        allow(DecodeJwt).to receive(:decode).and_return(json_data)
+        allow(User).to receive(:where).and_return(nil)
+        user = User.new
+        user.save!
+
+        result = user.change_attend_status(json_data)
+        expect(result).to eq 1
+      end
+    end
+
+    context 'when attend_status update is successful,' do
+      it 'returns 0.' do
+        allow(DecodeJwt).to receive(:decode).and_return(json_data)
+        user = User.new(email: 'test@test.com')
+        user.save!
+
+        result = user.change_attend_status(JSON.generate(json_data))
+        expect(result).to eq 0
+      end
+    end
+  end
+
+  describe 'change_allelgy_status' do
+    let(:json_data){
+      json_data = {
+        email: 'test@test.com',
+        user_id: '0123456789',
+        status: '0'
+      }
+      JSON.generate(json_data)
+    }
+
+    context 'when allelgy_status update failed,' do
+      it 'returns 1.' do
+        allow(DecodeJwt).to receive(:decode).and_return(json_data)
+        allow(User).to receive(:where).and_return(nil)
+        user = User.new
+        user.save!
+
+        result = user.change_allelgy_status(json_data)
+        expect(result).to eq 1
+      end
+    end
+
+    context 'when allelgy_status update is successful,' do
+      it 'returns 0.' do
+        allow(DecodeJwt).to receive(:decode).and_return(json_data)
+        user = User.new(email: 'test@test.com')
+        user.save!
+
+        result = user.change_allelgy_status(JSON.generate(json_data))
+        expect(result).to eq 0
+      end
     end
   end
 end
