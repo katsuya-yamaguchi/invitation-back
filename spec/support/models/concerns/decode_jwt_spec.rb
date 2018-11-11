eb273ff847a2e76256fad59ed656c6b20d9cace6
Katsuya003 <bb.win.3@gmail.com>
2018-11-11 15:59:02 +0900

    [update] DecodeJwtモジュールのテストを追加
    公開鍵取得メソッド、取得した公開鍵でトークン復号化メソッドのテストを追加した。

--- spec/support/models/concerns/decode_jwt_spec.rb
+++ spec/support/models/concerns/decode_jwt_spec.rb
@@ -1,21 +1,16 @@
 require 'rails_helper'
 
-require 'net/http'
-require 'uri'
-require 'json'
 require 'jwt'
 
-describe '#DecodeJwt' do
-  include DecodeJwt
-  context 'when collect jwt is sent,' do
-    it 'Decoded jwt is success.' do
-      token = 'eyJhbGciOiJSUzI1NiIsImtpZCI6IjBmNTVkZWZlOWU5YzU2ZmRhZTRkOGY0MDFjZjQ5Njc4YzE2N2MzYWEifQ.eyJpc3MiOiJodHRwczovL3NlY3VyZXRva2VuLmdvb2dsZS5jb20vaW52aXRhdGlvbi1jYTRiMiIsImF1ZCI6Imludml0YXRpb24tY2E0YjIiLCJhdXRoX3RpbWUiOjE1MzY2MDg4OTAsInVzZXJfaWQiOiJCQm8yOU9IOEtXUlQ4dmZPTTBJRVdvTzdVTmMyIiwic3ViIjoiQkJvMjlPSDhLV1JUOHZmT00wSUVXb083VU5jMiIsImlhdCI6MTUzNjgzODI1NSwiZXhwIjoxNTM2ODQxODU1LCJlbWFpbCI6ImJiLndpbi4zQGdtYWlsLmNvbSIsImVtYWlsX3ZlcmlmaWVkIjpmYWxzZSwiZmlyZWJhc2UiOnsiaWRlbnRpdGllcyI6eyJlbWFpbCI6WyJiYi53aW4uM0BnbWFpbC5jb20iXX0sInNpZ25faW5fcHJvdmlkZXIiOiJwYXNzd29yZCJ9fQ.GkJiYLNuvSU19OaCQ1ZtaBN9Vd5yrYBkLqH6UHQ1NzAw85wgECqMC0RZePNdb7NMwntHcwEVMhmSwTNmJtjZUXB5KMiVMoxEr97AetZZDJfeFiqepOjyL9qbF22nzrTqHY5DdyLU3kG_h_SWLFkGUzxFEa_rBFs8J80jkN6aywhm2jKf396OEn_pU4bpMrJly8X7BWrEIDxjo9edU0cA5KWbjryXE8IQBiAu8mBGK_ZlAfQLTZMYxLtrEbkOmLxY0q546bVcgef8wyBu-VEJJsze4aNQq1AfIQwe4xGP8ITJQvOnaWD1ndZayiz6iwXfEsmceAqGCeLqqOxQ1qvuXw'
-      expect( decode(token) ).not_to eq nil
-    end
-    it 'decode jwt is failed.' do
-      # tokenの頭文字にiを足している
-      token = 'ieyJhbGciOiJSUzI1NiIsImtpZCI6IjBmNTVkZWZlOWU5YzU2ZmRhZTRkOGY0MDFjZjQ5Njc4YzE2N2MzYWEifQ.eyJpc3MiOiJodHRwczovL3NlY3VyZXRva2VuLmdvb2dsZS5jb20vaW52aXRhdGlvbi1jYTRiMiIsImF1ZCI6Imludml0YXRpb24tY2E0YjIiLCJhdXRoX3RpbWUiOjE1MzY2MDg4OTAsInVzZXJfaWQiOiJCQm8yOU9IOEtXUlQ4dmZPTTBJRVdvTzdVTmMyIiwic3ViIjoiQkJvMjlPSDhLV1JUOHZmT00wSUVXb083VU5jMiIsImlhdCI6MTUzNjgzODI1NSwiZXhwIjoxNTM2ODQxODU1LCJlbWFpbCI6ImJiLndpbi4zQGdtYWlsLmNvbSIsImVtYWlsX3ZlcmlmaWVkIjpmYWxzZSwiZmlyZWJhc2UiOnsiaWRlbnRpdGllcyI6eyJlbWFpbCI6WyJiYi53aW4uM0BnbWFpbC5jb20iXX0sInNpZ25faW5fcHJvdmlkZXIiOiJwYXNzd29yZCJ9fQ.GkJiYLNuvSU19OaCQ1ZtaBN9Vd5yrYBkLqH6UHQ1NzAw85wgECqMC0RZePNdb7NMwntHcwEVMhmSwTNmJtjZUXB5KMiVMoxEr97AetZZDJfeFiqepOjyL9qbF22nzrTqHY5DdyLU3kG_h_SWLFkGUzxFEa_rBFs8J80jkN6aywhm2jKf396OEn_pU4bpMrJly8X7BWrEIDxjo9edU0cA5KWbjryXE8IQBiAu8mBGK_ZlAfQLTZMYxLtrEbkOmLxY0q546bVcgef8wyBu-VEJJsze4aNQq1AfIQwe4xGP8ITJQvOnaWD1ndZayiz6iwXfEsmceAqGCeLqqOxQ1qvuXw'
-      expect( decode(token) ).not_to eq nil
-    end
+describe 'decode' do
+  it 'successful decoding of token.' do
+    token = ENV['TOKEN']
+    expect(DecodeJwt.decode(token)).not_to eq nil
+  end
+end
+
+describe 'get_pubkey' do
+  it 'response is not nil.' do
+    expect(DecodeJwt.get_pubkey).not_to eq nil
   end
 end
